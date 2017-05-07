64eb0b5e5d1ce0bff18a053385d2d22663476045
Obi Uchenna David <daviduchenna@outlook.com>
2017-05-07 11:46:19 +0100

    added Company model

---
+++ app/src/main/java/com/davidmadethis/quicksend/models/Company.java
@@ -0,0 +1,45 @@
+package com.davidmadethis.quicksend.models;
+
+/**
+ * Created by root on 5/7/17.
+ */
+
+public class Company {
+
+    private String emailAddress;
+    private boolean editAble;
+    private String companyName;
+    private boolean shouldSend;
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public boolean isEditAble() {
+        return editAble;
+    }
+
+    public void setEditAble(boolean editAble) {
+        this.editAble = editAble;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public boolean isShouldSend() {
+        return shouldSend;
+    }
+
+    public void setShouldSend(boolean shouldSend) {
+        this.shouldSend = shouldSend;
+    }
+}
